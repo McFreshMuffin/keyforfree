5744b8d95226a37a51216aca81ae1300927ab329
Tobi <BlackDeath@landing-zone.de>
2019-03-12 17:43:17 +0100

    Delete Funktion eingefügt

--- src/main/java/de/dhbw/karlsruhe/webprojekt/servlet/CartServlet.java
+++ src/main/java/de/dhbw/karlsruhe/webprojekt/servlet/CartServlet.java
@@ -28,129 +28,76 @@ public class CartServlet extends HttpServlet {
     @EJB
     BestellungBean bestellungBean;
 
-    /*
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        String action = request.getParameter("action");
-        int gameId = Integer.parseInt(request.getParameter("id"));
-        String currentPage = request.getParameter("currentUrl");
-        HttpSession session = request.getSession();
-        List<Games> shoppingCart = (ArrayList) session.getAttribute("shoppingCart");
-        
-        switch (action) {
-            case "add":
-                Games game = this.gameBean.findGameById(gameId);
-                shoppingCart.add(game);
-                request.setAttribute("cart", shoppingCart);
-                request.setAttribute("currentPage", currentPage);
-                session.setAttribute("shoppingCart", shoppingCart);
-                request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
-                break;
-            /*case "delete":
-                cart = sessionCart;
-                for (Games curGame : cart) {
-                    if (curGame.getGameId() == gameId) {
-                        cart.remove(curGame);
-                        request.setAttribute("cart", cart);
-                        request.setAttribute("currentPage", currentPage);
-                        session.setAttribute("sessionCart", cart);
-                        request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
-                    }
-                }
-                break;
-        }
-     */
- /*
-        if (session.getAttribute("userId") != null) {
-            ArrayList mycart = (ArrayList) session.getAttribute("itemlist");
-            double value = (Double) session.getAttribute("total");
-            int gameId = Integer.parseInt(request.getParameter("gameId"));
-            Games game = this.gameBean.findGameById(gameId);
-            if (game != null) {
-                value += game.getPriceFinal();
-                mycart.add(game);
-                session.setAttribute("itemlist", mycart);
-                session.setAttribute("total", value);
-                response.sendRedirect("shop.jsp");
-            }
-        } else {
-            response.sendRedirect("index.jsp");
-        }
-          {
+    private HttpSession session = null;
+    private List<Games> shoppingCart = null;
+    private String currentPage = null;
+    private int gameId;
 
-        }
-    }*/
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
-        HttpSession session = request.getSession();
-        List<Games> shoppingCart = (ArrayList) session.getAttribute("shoppingCart");
+        initSession(request);
         request.setAttribute("cart", shoppingCart);
         request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
         String action = request.getParameter("action");
-        String currentPage = request.getParameter("currentUrl");
-        HttpSession session = request.getSession();
-        List<Games> shoppingCart = (ArrayList) session.getAttribute("shoppingCart");
+        currentPage = request.getParameter("currentUrl");
+        gameId = Integer.parseInt(request.getParameter("id"));
+        initSession(request);
 
         switch (action) {
             case "add":
-                int gameId = Integer.parseInt(request.getParameter("id"));
-                Games game = this.gameBean.findGameById(gameId);
-                shoppingCart.add(game);
-                request.setAttribute("cart", shoppingCart);
-                request.setAttribute("currentPage", currentPage);
-                session.setAttribute("shoppingCart", shoppingCart);
-                request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
+                addGame(request, response);
                 break;
             case "buy":
-                long userId = (long) session.getAttribute("userId");
-                Benutzer benutzer = (Benutzer) session.getAttribute("user");
-                double totalPrice = Double.parseDouble(request.getParameter("totalPrice"));
-                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-                Date date = new Date();
-                Bestellung b = new Bestellung(userId, totalPrice, date, benutzer, shoppingCart);
-                this.bestellungBean.saveBestellung(b);
-                session.setAttribute("shoppingCart", new ArrayList<Games>());
-                request.getRequestDispatcher("/WEB-INF/success.jsp").forward(request, response);
+                buyCart(request, response);
                 break;
-            /*case "delete":
-                cart = sessionCart;
-                for (Games curGame : cart) {
-                    if (curGame.getGameId() == gameId) {
-                        cart.remove(curGame);
-                        request.setAttribute("cart", cart);
-                        request.setAttribute("currentPage", currentPage);
-                        session.setAttribute("sessionCart", cart);
-                        request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
-                    }
-                }
-                break;*/
+            case "delete":
+                deleteGame(request, response);
+                break;
         }
     }
 
+    private void initSession(HttpServletRequest request) {
+        session = request.getSession();
+        shoppingCart = (ArrayList) session.getAttribute("shoppingCart");
+    }
+
+    private void deleteGame(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        for (Games curGame : shoppingCart) {
+            if (curGame.getGameId() == gameId) {
+                shoppingCart.remove(curGame);
+                request.setAttribute("cart", shoppingCart);
+                request.setAttribute("currentPage", currentPage);
+                session.setAttribute("sessionCart", shoppingCart);
+                request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
+            }
+        }
+    }
+
+    private void addGame(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Games game = this.gameBean.findGameById(gameId);
+        shoppingCart.add(game);
+        request.setAttribute("cart", shoppingCart);
+        request.setAttribute("currentPage", currentPage);
+        session.setAttribute("shoppingCart", shoppingCart);
+        request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
+    }
+
+    private void buyCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long userId = (long) session.getAttribute("userId");
+        Benutzer benutzer = (Benutzer) session.getAttribute("user");
+        double totalPrice = Double.parseDouble(request.getParameter("totalPrice"));
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        Bestellung b = new Bestellung(userId, totalPrice, date, benutzer, shoppingCart);
+        this.bestellungBean.saveBestellung(b);
+        session.setAttribute("shoppingCart", new ArrayList<Games>());
+        request.getRequestDispatcher("/WEB-INF/success.jsp").forward(request, response);
+    }
 }
