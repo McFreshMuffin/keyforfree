a542c1d5f7094ccd3e6d0cf9f11bf3d48793c72b
Tobi <BlackDeath@landing-zone.de>
2019-03-24 12:58:26 +0100

    Warenkorb Design und Funktionalität

--- src/main/java/de/dhbw/karlsruhe/webprojekt/servlet/CartServlet.java
+++ src/main/java/de/dhbw/karlsruhe/webprojekt/servlet/CartServlet.java
@@ -7,23 +7,18 @@ import de.dhbw.karlsruhe.webprojekt.email.ObjectConverter;
 import de.dhbw.karlsruhe.webprojekt.model.Benutzer;
 import de.dhbw.karlsruhe.webprojekt.model.Bestellung;
 import de.dhbw.karlsruhe.webprojekt.model.Games;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.xml.transform.TransformerException;
-import org.apache.fop.apps.FOPException;
 
 @WebServlet(name = "CartServlet", urlPatterns = {"/cart"})
 public class CartServlet extends HttpServlet {
@@ -43,7 +38,11 @@ public class CartServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         initSession(request);
+        String reqRecordsPerPage = request.getParameter("recordsPerPage");
+        String reqCurrentPage = request.getParameter("currentPage");
         request.setAttribute("cart", shoppingCart);
+        request.setAttribute("recordsPerPage", reqRecordsPerPage);
+        request.setAttribute("currentPage", reqCurrentPage);
         request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
     }
 
@@ -74,25 +73,28 @@ public class CartServlet extends HttpServlet {
         shoppingCart = (ArrayList) session.getAttribute("shoppingCart");
     }
 
-    private void deleteGame(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        for (Games curGame : shoppingCart) {
-            if (curGame.getGameId() == gameId) {
-                shoppingCart.remove(curGame);
-                request.setAttribute("cart", shoppingCart);
-                request.setAttribute("currentPage", currentPage);
-                session.setAttribute("sessionCart", shoppingCart);
-                request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
-            }
-        }
-    }
-
     private void addGame(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         Games game = this.gameBean.findGameById(gameId);
         shoppingCart.add(game);
         request.setAttribute("cart", shoppingCart);
         request.setAttribute("currentPage", currentPage);
         session.setAttribute("shoppingCart", shoppingCart);
+        //request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
+
+        response.sendRedirect("cart?" + currentPage);
+    }
+
+    private void deleteGame(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Games game = new Games();
+        for (Games curGame : shoppingCart) {
+            if (curGame.getGameId() == gameId) {
+                game = curGame;
+            }
+        }
+        shoppingCart.remove(game);
+        request.setAttribute("cart", shoppingCart);
+        request.setAttribute("currentPage", currentPage);
+        session.setAttribute("sessionCart", shoppingCart);
         request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
     }
 
@@ -102,15 +104,13 @@ public class CartServlet extends HttpServlet {
         double totalPrice = Double.parseDouble(request.getParameter("totalPrice"));
         Date date = new Date();
         Bestellung b = new Bestellung(userId, totalPrice, date, benutzer, shoppingCart);
-
+        this.bestellungBean.saveBestellung(b);
         ObjectConverter converter = new ObjectConverter();
         converter.convertToXml(b);
         converter.convertXmlToPdf(b.getBestellId());
         EmailSendingService ess = new EmailSendingService();
         ess.createEmail("service.key4free@gmail.com", benutzer.getNachname(), b.getBenutzerId());
         ess.sendEmail();
-
-        this.bestellungBean.saveBestellung(b);
         session.setAttribute("shoppingCart", new ArrayList<Games>());
         request.getRequestDispatcher("/WEB-INF/success.jsp").forward(request, response);
     }
